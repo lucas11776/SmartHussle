0379b7ed67559e44a3f0bfb85fd4f1c38f94ec6d
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-11-02 07:59:56 +0200

    Added Bootstrap For Developement

---
+++ application/views/Register.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+
+    <title>Smart Hussle</title>
+</head>
+
+<body>
+    
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+</body>
+
+</html>
