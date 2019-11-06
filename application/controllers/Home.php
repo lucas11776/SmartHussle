314be6fd9e1ff5ec49a80f98d2a97e10e8ab6380
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-11-06 12:33:19 +0200

    Added ProductPriview Template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
+
+	/**
+	 * Request database limit
+	 */
+	protected const LIMIT = 6;
 
 	/**
 	 * Home Controller
@@ -10,6 +16,10 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('home');
+		$page = [
+			'categories' => $this->categories->get(),
+			'products' => $this->products->get(null, self::LIMIT),
+		];
+		$this->load->view('home', $page);
 	}
 }
---
+++ application/views/templates/product_preview.php
@@ -0,0 +1,22 @@
+<div class="col-xl-4 col-md-6">
+    <div class="product">
+        <div class="product_image"><img src="assets/images/product_1.jpg" alt=""></div>
+        <div class="product_content">
+            <div class="product_info d-flex flex-row align-items-start justify-content-start">
+                <div>
+                    <div>
+                        <div class="product_name"><a href="product.html">Cool Clothing with Brown Stripes</a></div>
+                        <div class="product_category"><a href="category.html">Category</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="product_buttons">
+                <div class="text-right d-flex flex-row align-items-start justify-content-start" title="Order Online...">
+                    <div class="product_button product_cart text-center d-flex flex-column align-items-center justify-content-center">
+                        <div><div><img src="assets/images/cart.svg" class="svg" alt=""><div>+</div></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
