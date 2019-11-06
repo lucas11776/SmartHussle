82860d76c7e953fc6a2828a2a373483a7b972734
Themba Lucas Ngubeni <thembangubeni04@gmail.com>
2019-11-06 16:59:43 +0200

    Added Footer Template

---
+++ application/views/templates/Footer.php
@@ -0,0 +1,80 @@
+<footer class="footer">
+    <div class="footer_content">
+        <div class="container">
+            <div class="row">
+                
+                <!-- About -->
+                <div class="col-lg-4 footer_col">
+                    <div class="footer_about">
+                        <div class="footer_logo">
+                            <a href="#">
+                                <div class="d-flex flex-row align-items-center justify-content-start">
+                                    <div class="footer_logo_icon"><img src="assets/images/logo_2.png" alt=""></div>
+                                    <div>SmartHussle</div>
+                                </div>
+                            </a>		
+                        </div>
+                        <div class="footer_about_text">
+                            <p>
+                                description about SmartHussle all details about company.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Footer Links -->
+                <div class="col-lg-4 footer_col">
+                    <div class="footer_menu">
+                        <div class="footer_title">Category</div>
+                        <ul class="footer_list">
+                            <?php for ($i = 0; $i < (count($categories) > 4 ? 4 : count($categories)); $i++): ?>
+                                <li>
+                                    <a href="<?= base_url('categories/' . $categories[$i]['slug']) ?>" class="text-capitalize">
+                                        <div><?= $categories[$i]['name'] ?></div>
+                                    </a>
+                                </li>
+                            <?php endfor; ?>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Footer Contact -->
+                <div class="col-lg-4 footer_col">
+                    <div class="footer_contact">
+                        <div class="footer_social">
+                            <div class="footer_title">Social</div>
+                            <ul class="footer_social_list d-flex flex-row align-items-start justify-content-start">
+                                <li><a href="<?= base_url('') ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                <li><a href="<?= base_url('') ?>"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                                <li><a href="<?= base_url('') ?>"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                                <li><a href="<?= base_url('') ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_bar">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="footer_bar_content d-flex flex-md-row flex-column align-items-center justify-content-start">
+                        <div class="copyright order-md-1 order-2"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+                        <nav class="footer_nav ml-md-auto order-md-2 order-1">
+                            <ul class="d-flex flex-row align-items-center justify-content-start">
+                                <li><a href="<?= base_url('') ?>">Home</a></li>
+                                <li><a href="<?= base_url('store') ?>">Store</a></li>
+                                <li><a href="<?= base_url('register') ?>">Register</a></li>
+                                <li><a href="<?= base_url('login') ?>">Login</a></li>
+                                <li><a href="<?= base_url('contact') ?>">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- application/views/templates/product_preview.php
+++ application/views/templates/product_preview.php
@@ -1,19 +1,35 @@
 <div class="col-xl-4 col-md-6">
     <div class="product">
-        <div class="product_image"><img src="assets/images/product_1.jpg" alt=""></div>
+        <div class="product_image">
+            <img src="<?= base_url($picture) ?>" alt="<?= $name ?>">
+        </div>
         <div class="product_content">
             <div class="product_info d-flex flex-row align-items-start justify-content-start">
                 <div>
                     <div>
-                        <div class="product_name"><a href="product.html">Cool Clothing with Brown Stripes</a></div>
-                        <div class="product_category"><a href="category.html">Category</a></div>
+                        <div class="product_name">
+                            <a class="text-capitalize" href="<?= base_url('store/' . $slug) ?>"><?= $name ?></a>
+                        </div>
+                        <div class="product_category">
+                            Category : <a class="text-capitalize" href="<?= base_url('categories/' . $category) ?>)"><?= $category ?></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="ml-auto text-right">
+                    <!-- Removed home_item_rating -->
+                    <div class="product_price text-right">
+                        R<?= floor($price) ?><span>.99</span>
                     </div>
                 </div>
             </div>
             <div class="product_buttons">
-                <div class="text-right d-flex flex-row align-items-start justify-content-start" title="Order Online...">
+                <div class="text-right d-flex flex-row align-items-start justify-content-start" title="Order Online Now...">
                     <div class="product_button product_cart text-center d-flex flex-column align-items-center justify-content-center">
-                        <div><div><img src="assets/images/cart.svg" class="svg" alt=""><div>+</div></div></div>
+                        <div>
+                            <div>
+                                <img src="<?= base_url('assets/images/cart.svg') ?>" class="svg" alt="Order Online..."><div>+</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
